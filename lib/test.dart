da2b6a12ea330b0bd390363e415aa0f89a36472a
bhaviVtech <bhavisha.vtechglobe@gmail.com>
2023-06-14 11:04:34 +0530

    test

---
+++ lib/test.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Testclass extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return TestclassState();
+  }
+}
+
+class TestclassState extends State<Testclass> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      body: SafeArea(
+        child: Container(),
+      ),
+    );
+  }
+}
